40f057659ab2bfbe94e9c40eaf0deebcb0ab28e5
ligegexiao <1527996509@qq.com>
2025-04-05 13:21:16 +0800

    Site updated: 2025-04-05 13:21:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/第一篇文章/","2025/04/03/hello-world/"];function toRandomPost(){
+var posts=["undefined/3eab.html","undefined/3eeb.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
